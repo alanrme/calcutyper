e7cdd8d435b31810f4e65f2b955e4725cbb4d7c3
Alan <alanrd772@gmail.com>
2025-01-10 02:08:02 +0400

    Edit homepage content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to Calcutyper</h1>
+<p><strong>Click your calculator model above to enter the editor.</strong></p>
+<p>
+    Keymap fonts from the <a href="https://edu.casio.com/forteachers/er/fontsets/">Casio website</a>.
+    Calculator screen fonts are property of Casio Computer Co., Ltd. and were manually remade in <a href="https://www.fontstruct.com/">FontStruct</a> by me.
+    Website made by <a href="https://alanr.me">Alan</a>.
+</p>
