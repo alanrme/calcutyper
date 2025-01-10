283d52fb7151d3c50e98f667868e2be9ea2534eb
Alan <alanrd772@gmail.com>
2025-01-10 13:36:48 +0400

    Prepare for github pages deployment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	let { children } = $props();
+    import { base } from '$app/paths';
 </script>
 
 <nav class="bg-slate-300 fixed w-full top-0 left-0">
@@ -9,10 +10,10 @@
 			<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 				<div class="hidden sm:ml-6 sm:block">
 					<div class="flex space-x-4">
-						<a href="/" class="rounded-md bg-slate-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-						<a href="/991ex" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-slate-700 hover:text-white">fx-991EX</a>
-						<a href="/991esp" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-slate-700 hover:text-white">fx-991ES PLUS</a>
-						<a href="/991ms" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-slate-700 hover:text-white">fx-991MS</a>
+						<a href="{base}/" class="rounded-md bg-slate-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+						<a href="{base}/991ex" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-slate-700 hover:text-white">fx-991EX</a>
+						<a href="{base}/991esp" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-slate-700 hover:text-white">fx-991ES PLUS</a>
+						<a href="{base}/991ms" class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-slate-700 hover:text-white">fx-991MS</a>
 					</div>
 				</div>
 			</div>
