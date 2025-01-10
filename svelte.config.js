dbed94f01a01e8390e181eea7e30eba987da1c3f
Alan <alanrd772@gmail.com>
2025-01-10 13:43:04 +0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/calcutyper' : '',
-        }
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+
 	}
 };
 
